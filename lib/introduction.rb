4044647613f982c3ac25ca7d93f006d2f8eeb2d5
Logan Smith <logsmith06@gmail.com>
2019-10-08 17:13:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,3 +2,9 @@ name = "Josh"
 def introduction(name)
   puts "Hi, my name is #{name}.\n"
 end
+
+name = "Dan"
+language = "Ember"
+def introduction_with_language(language, name)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
